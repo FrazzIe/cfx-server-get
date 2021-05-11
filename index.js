9a41b3ce51e466e3f912bf82ece6f4d0c7300198
Fraser Watt <28698343+FrazzIe@users.noreply.github.com>
2021-05-11 01:38:56 +0100

    add basic argument validation

---
+++ index.js
@@ -0,0 +1,54 @@
+//imports
+const fs = require("fs");
+
+//vars
+const appName = "cfx-server-get";
+const numArgs = 2;
+const helpMsg = `
+	${appName} usage:
+	node index.js <version> <output directory>
+		<version>:
+			- recommended
+			- latest
+			- critical
+		<output directory>:
+			- C:\\Path\\To\\File
+`;
+const versions = ["recommended", "latest", "critical"];
+let args = process.argv.slice(2);
+let version = args[0];
+let output = args[1];
+
+function log(tag, msg) {
+	console.log(`${appName}/${tag}: ${msg}`);
+}
+
+if (args.length == 0) {
+	log("INFO", helpMsg);
+	return;
+}
+
+if (args.length <= 1) {
+	log("ERROR", `Invalid arguments (got: ${args.length} expected: ${numArgs})`);
+	return;
+}
+
+if (!versions.includes(version)) {
+	log("ERROR", `${version} is an invalid <version> (versions: ${versions.join(", ")})`);
+	return;
+}
+
+let pathInfo;
+
+try {
+	pathInfo = fs.statSync(output);
+} catch(error) {
+	log("ERROR", `${output} is an invalid <output directory>`);
+	return;
+}
+
+if (!pathInfo.isDirectory()) {
+	log("ERROR", `${output} is an invalid <output directory>`);
+}
+
+log("SUCCESS", "DO STUFF");
